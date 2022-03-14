deb3d85e04cfb3396cf4ea7b26dc3c4790634177
anonymous anonymous <katherinetheiss@gmail.com>
2022-03-14 17:08:31 +0000

    Display associated collections on show pages

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -42,8 +42,14 @@ class ActionsController < ApplicationController
   # DELETE /actions/1
   def destroy
     @action.destroy
-    redirect_to actions_url, notice: 'Action was successfully destroyed.'
+    message = "Action was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to actions_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
