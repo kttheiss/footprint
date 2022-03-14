c0718a6408f5b8ee5d5d3a90ceafb4ba8b6e5d4a
anonymous anonymous <katherinetheiss@gmail.com>
2022-03-14 17:09:16 +0000

    Remove controller comments

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -4,25 +4,20 @@ class ActionsController < ApplicationController
 
   before_action :set_action, only: %i[show edit update destroy]
 
-  # GET /actions
   def index
     @q = Action.ransack(params[:q])
     @actions = @q.result(distinct: true).includes(:user,
                                                   :category).page(params[:page]).per(10)
   end
 
-  # GET /actions/1
   def show; end
 
-  # GET /actions/new
   def new
     @action = Action.new
   end
 
-  # GET /actions/1/edit
   def edit; end
 
-  # POST /actions
   def create
     @action = Action.new(action_params)
 
@@ -38,7 +33,6 @@ class ActionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /actions/1
   def update
     if @action.update(action_params)
       redirect_to @action, notice: "Action was successfully updated."
@@ -47,7 +41,6 @@ class ActionsController < ApplicationController
     end
   end
 
-  # DELETE /actions/1
   def destroy
     @action.destroy
     message = "Action was successfully deleted."
@@ -68,12 +61,10 @@ class ActionsController < ApplicationController
     end
   end
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_action
     @action = Action.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def action_params
     params.require(:action).permit(:date, :name, :category_id,
                                    :impact_level_value, :impact_level, :user_id, :description)
